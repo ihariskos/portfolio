e0f9ffe1ad3126c47b2aa827f9af5b7ecec96d9f
ihariskos <ioannis.hariskos@gmail.com>
2019-06-11 06:21:02 +0200

    theme btn mod, added footer, fixed header mobile

--- js/theme-changer.js
+++ js/theme-changer.js
@@ -1,19 +1,37 @@
 const darkTheme = 'dark-theme';
-
-document.querySelector('.header__btn-theme').addEventListener('click', function () {    
-    const darkThemeEnabled = [
-        document.body.classList.toggle(darkTheme),
-        document.querySelector('.header__btn-theme').classList.toggle(darkTheme),
-        document.querySelector('.header__logo').classList.toggle(darkTheme),
-        document.querySelector('.copylove__link').classList.toggle(darkTheme)
-    ];
-
-    localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
+const themeButtons = document.querySelectorAll('.theme-btn').forEach(el => {
+    el.addEventListener('click', function () {    
+        const darkThemeEnabled = [
+            document.body.classList.toggle(darkTheme),
+            document.querySelector('.header__btn-theme').classList.toggle(darkTheme),
+            document.querySelector('.footer__btn-theme').classList.toggle(darkTheme),
+            document.querySelector('.header__logo').classList.toggle(darkTheme),
+            document.querySelector('.footer__logo').classList.toggle(darkTheme),
+            document.querySelector('.footer__link').classList.toggle(darkTheme)
+        ];
+    
+        localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
+    }); 
 });
+
+// themeButtons.addEventListener('click', function () {    
+//     const darkThemeEnabled = [
+//         document.body.classList.toggle(darkTheme),
+//         document.querySelector('.header__btn-theme').classList.toggle(darkTheme),
+//         document.querySelector('.footer__btn-theme').classList.toggle(darkTheme),
+//         document.querySelector('.header__logo').classList.toggle(darkTheme),
+//         document.querySelector('.footer__logo').classList.toggle(darkTheme),
+//         document.querySelector('.footer__link').classList.toggle(darkTheme)
+//     ];
+
+//     localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
+// });
 
 if (JSON.parse(localStorage.getItem('dark-theme-enabled').split(',')[0])) {
     document.body.classList.add(darkTheme);
     document.querySelector('.header__btn-theme').classList.add(darkTheme);
+    document.querySelector('.footer__btn-theme').classList.toggle(darkTheme);
     document.querySelector('.header__logo').classList.add(darkTheme);
-    document.querySelector('.copylove__link').classList.add(darkTheme);
+    document.querySelector('.footer__logo').classList.toggle(darkTheme);
+    document.querySelector('.footer__link').classList.add(darkTheme);
 }

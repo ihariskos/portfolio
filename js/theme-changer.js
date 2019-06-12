71d7d7787f942b63fb349485f118f5063ae2bf1e
ihariskos <ioannis.hariskos@gmail.com>
2019-06-12 22:15:27 +0200

    gallery mobile opacity, refactor, minified images

--- js/theme-changer.js
+++ js/theme-changer.js
@@ -14,19 +14,6 @@ const themeButtons = document.querySelectorAll('.theme-btn').forEach(el => {
     }); 
 });
 
-// themeButtons.addEventListener('click', function () {    
-//     const darkThemeEnabled = [
-//         document.body.classList.toggle(darkTheme),
-//         document.querySelector('.header__btn-theme').classList.toggle(darkTheme),
-//         document.querySelector('.footer__btn-theme').classList.toggle(darkTheme),
-//         document.querySelector('.header__logo').classList.toggle(darkTheme),
-//         document.querySelector('.footer__logo').classList.toggle(darkTheme),
-//         document.querySelector('.footer__link').classList.toggle(darkTheme)
-//     ];
-
-//     localStorage.setItem('dark-theme-enabled', darkThemeEnabled);
-// });
-
 if (JSON.parse(localStorage.getItem('dark-theme-enabled').split(',')[0])) {
     document.body.classList.add(darkTheme);
     document.querySelector('.header__btn-theme').classList.add(darkTheme);
